2601f0dc942d65fca7cfbe6b5f9aadc63d5be62e
nidakircali <nidakircali@gmail.com>
2023-03-26 00:32:38 +0300

    Update MainActivity.kt
    Added "The denominator cannot be 0 in the division" massage.

--- Calculator2/app/src/main/java/com/nidakircali/calculator/MainActivity.kt
+++ Calculator2/app/src/main/java/com/nidakircali/calculator/MainActivity.kt
@@ -58,27 +58,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun division (view: View){
-
-        val firstNumber = findViewById<TextView>(R.id.firstNumber)
-        number1 = firstNumber.text.toString().toIntOrNull()
-
-        val secondNumber = findViewById<TextView>(R.id.secondNumber)
-        number2 = secondNumber.text.toString().toIntOrNull()
-
-        val resultBox = findViewById<TextView>(R.id.resultBox)
-
-
-        if (number1== null || number2==null){
-            resultBox.text="You should enter two numbers!"
-
-        } else{
-            result =number1!! * number2!!
-            resultBox.text="${result}"
-        }
-
-    }
-
     fun multiplication (view: View){
 
         val firstNumber = findViewById<TextView>(R.id.firstNumber)
@@ -92,8 +71,33 @@ class MainActivity : AppCompatActivity() {
 
         if (number1== null || number2==null){
             resultBox.text="You should enter two numbers!"
+        }
+            else{
+            result =number1!! * number2!!
+            resultBox.text="${result}"
+        }
 
-        } else{
+    }
+
+    fun division (view: View){
+
+        val firstNumber = findViewById<TextView>(R.id.firstNumber)
+        number1 = firstNumber.text.toString().toIntOrNull()
+
+        val secondNumber = findViewById<TextView>(R.id.secondNumber)
+        number2 = secondNumber.text.toString().toIntOrNull()
+
+        val resultBox = findViewById<TextView>(R.id.resultBox)
+
+
+        if (number1== null || number2==null){
+            resultBox.text="You should enter two numbers!" }
+
+        else if (number2==0) {
+            resultBox.text="Denominator cannot be 0 in division!"
+
+        }
+        else{
             result =number1!! / number2!!
             resultBox.text="${result}"
         }
